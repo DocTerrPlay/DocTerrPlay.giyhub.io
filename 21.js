974ed68478bdad42ced8b13407988c2b16394a59
DocTerr <146622833+DocTerrPlay@users.noreply.github.com>
2026-01-29 00:45:20 +0300

    Update 21.js

--- 21.js
+++ 21.js
@@ -146,3 +146,10 @@ function reduceAce(playerSum, playerAceCount) {
     return playerSum;
 }
 
+<script>
+// Когда пользователь нажимает на <div>, откройте всплывающее окно
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
